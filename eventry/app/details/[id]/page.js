5e9ea310cf720d5b8a7351d2c196bf42d645eb2b
MohammedNawazMynulIslam <mynulislam183@gmail.com>
2026-01-24 17:39:45 +0600

    finished hero and event sections implementations using data from mongodb

---
+++ eventry/app/details/[id]/page.js
@@ -0,0 +1,21 @@
+import HeroSection from "@/components/details/HeroSection"
+import EventDetails from "@/components/details/EventDetails"
+import EventVenue from "@/components/details/EventVenue"
+import { getEventById } from "@/db/queries"
+
+const page = async ({params: {id}}) => {
+  const eventInfo = await getEventById(id)
+  return (
+    <>
+      <HeroSection eventInfo={eventInfo} />
+      <section class="container">
+        <div class="grid grid-cols-5 gap-12 my-12">
+          <EventDetails details={eventInfo?.details} swags={eventInfo?.swags} />
+          <EventVenue location={eventInfo?.location} />
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default page
---
+++ eventry/db/queries.js
@@ -0,0 +1,16 @@
+import {eventModel} from "@/app/models/events-models"
+import replaceMongoIdInArray, { replaceMongoIdInObject } from "@/utils/data-utils"
+
+
+async function getAllEvents(){
+    const allEvents = await eventModel.find({}).lean()
+    return replaceMongoIdInArray(allEvents)
+ }
+async function getEventById(eventId){
+    const event = await eventModel.findById(eventId).lean()
+    return replaceMongoIdInObject(event)
+ }
+export {
+    getAllEvents,
+    getEventById
+}
